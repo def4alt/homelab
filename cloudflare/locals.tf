13b3a55cb777e6ffa554fe1a0fd008347a1c1f14
Andrii Olkhovych <andrii.olkhovych@tum.de>
2026-07-21 18:58:24 +0200

    fix(dns): move slskd from tailnet to public tunnel as soulseek.def4alt.com

--- cloudflare/locals.tf
+++ cloudflare/locals.tf
@@ -13,6 +13,7 @@ locals {
     "sea.${var.base_domain}",
     "yt.${var.base_domain}",
     "music.${var.base_domain}",
+    "soulseek.${var.base_domain}",
     var.base_domain,
   ])
 
@@ -25,7 +26,6 @@ locals {
     "prowlarr.${var.base_domain}",
     "sonarr.${var.base_domain}",
     "radarr.${var.base_domain}",
-    "slskd.${var.base_domain}",
     "lidarr.${var.base_domain}",
   ])
 
@@ -36,7 +36,6 @@ locals {
     "prowlarr.${var.base_domain}"   = "perun.tail6f3b0.ts.net"
     "radarr.${var.base_domain}"     = "perun.tail6f3b0.ts.net"
     "sonarr.${var.base_domain}"     = "perun.tail6f3b0.ts.net"
-    "slskd.${var.base_domain}"      = "perun.tail6f3b0.ts.net"
     "lidarr.${var.base_domain}"     = "perun.tail6f3b0.ts.net"
   }
 
